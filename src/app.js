709d69cedb8d3885f948cbfd3b2c167316872a96
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-04-15 10:33:50 +0530

    Add CORS configuration to Express server using environment variable

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
